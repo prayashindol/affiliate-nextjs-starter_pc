434609fa25cd8848f30c7d844cb7eca22452ecc5
prayashindol <46970018+prayashindol@users.noreply.github.com>
2025-07-27 12:53:04 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -152,7 +152,7 @@ export default function Example() {
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
               <div className="mx-auto max-w-2xl text-center">
                 <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-                  The Ultimate Resource Hub for Airbnb Hosts
+                  The Ultimate Resource Hub<br />for Airbnb Hosts
                 </h1>
                 <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
                   Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt
