97a3d08d3976a0dff38ad360a5a1a4427c2e3cb6
prayashindol <46970018+prayashindol@users.noreply.github.com>
2025-07-28 00:21:18 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -191,7 +191,7 @@ export default function Example() {
 </div>
               
         </div>
-             <ToolsDirectory />
+             <ToolsDirectory records={records} />
               <div className="mt-16 flow-root sm:mt-24">
                 <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-gray-900/10 ring-inset lg:-m-4 lg:rounded-2xl lg:p-4">
                   <img
