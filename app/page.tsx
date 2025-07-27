f7bb568593b5410f0fdd0c00cf4533228a77da18
prayashindol <46970018+prayashindol@users.noreply.github.com>
2025-07-27 13:03:09 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -155,8 +155,7 @@ export default function Example() {
                   The Ultimate Resource Hub<br />for Airbnb Hosts
                 </h1>
                 <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
-                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt
-                  amet fugiat veniam occaecat.
+                  Your central reliable resource for essential tools, templates, and training to maximize your hosting success
                 </p>
                 <div className="mt-10 flex items-center justify-center gap-x-6">
                   <a
