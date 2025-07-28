7d5cde12559b9abf5ef0105194413f44a82cb78f
prayashindol <prayas.chaudhary@gmail.com>
2025-07-28 23:20:43 +0100

    Create page.js

---
+++ products/page.js
@@ -0,0 +1,10 @@
+export default function ProductsPage() {
+  return (
+    <div>
+      <div className="bg-blue-500 text-white text-2xl p-8">
+        TAILWIND IS WORKING
+      </div>
+      {/* Your other product code below */}
+    </div>
+  )
+}
