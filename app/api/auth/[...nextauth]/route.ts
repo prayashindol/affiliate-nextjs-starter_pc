8e33fc6c3eb10ab54f5f7853c831c25d1f1bc069
prayashindol <prayas.chaudhary@gmail.com>
2025-08-14 21:35:46 +0100

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,7 @@ import GoogleProvider from "next-auth/providers/google"
 import GitHubProvider from "next-auth/providers/github"
 
 const handler = NextAuth({
+  secret: process.env.NEXTAUTH_SECRET, // <-- Explicitly set the secret here
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
